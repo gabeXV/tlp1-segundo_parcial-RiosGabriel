9c97e15e28d80c97926d5ecb8ccd689426bc3a0c
gabyr <gabrielitorg@gmail.com>
2024-06-27 15:05:07 -0300

    se crean los middlewares y se obtienen los productos

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ const express = require("express");
 const db = require("./db");
 const app = express();
 
+app.use(express.json());
+
+app.get("/", (req,res) =>{
+    res.send("pagina de inicio");
+});
+
+app.get("/products", (req,res) =>{
+    res.json(db);
+});
 
 
 
